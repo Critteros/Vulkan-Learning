5d7e44bd2eb7bcf2f737f601b0c7a5244d819e03
Critteros <szymonkoziol3@gmail.com>
2021-07-13 16:51:51 +0200

    Finally ready to use version

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
@@ -7,7 +9,8 @@
 #include <glm/mat4x4.hpp>
 
 #include <vulkan/vulkan.h>
-#include <fmt/core.h>
+#include <spdlog/spdlog.h>
+// #include <fmt/core.h>
 
 #include <iostream>
 
@@ -23,6 +26,15 @@ int main()
 
     fmt::print("Hello, world!\n");
     std::cout << extensionCount << " extensions supported\n";
+
+    spdlog::info("Welcome to spdlog!");
+    spdlog::error("Some error message with arg: {}", 1);
+
+    spdlog::warn("Easy padding in numbers like {:08d}", 12);
+    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    spdlog::info("Support for floats {:03.2f}", 1.23456);
+    spdlog::info("Positional args are {1} {0}..", "too", "supported");
+    spdlog::info("{:<30}", "left aligned");
 
     glm::mat4 matrix;
     glm::vec4 vec;
