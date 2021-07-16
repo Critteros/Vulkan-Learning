ba38fb6649977149339d3eea0b3bfe45de348823
Critteros <szymonkoziol3@gmail.com>
2021-07-17 00:47:20 +0200

    Fixed self referencing issue in pipeline.cpp

--- src/pipeline.hpp
+++ src/pipeline.hpp
@@ -11,15 +11,14 @@ namespace engine
 
     struct PipelineConfigInfo
     {
-        VkViewport viewport{};
-        VkRect2D scissor{};
-        VkPipelineViewportStateCreateInfo viewportInfo{};
-        VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo{};
-        VkPipelineRasterizationStateCreateInfo rasterizationInfo{};
-        VkPipelineMultisampleStateCreateInfo multisampleInfo{};
-        VkPipelineColorBlendAttachmentState colorBlendAttachment{};
-        VkPipelineColorBlendStateCreateInfo colorBlendInfo{};
-        VkPipelineDepthStencilStateCreateInfo depthStencilInfo{};
+        VkViewport viewport;
+        VkRect2D scissor;
+        VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+        VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+        VkPipelineMultisampleStateCreateInfo multisampleInfo;
+        VkPipelineColorBlendAttachmentState colorBlendAttachment;
+        VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+        VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
         VkPipelineLayout pipelineLayout = nullptr;
         VkRenderPass renderPass = nullptr;
         uint32_t subpass = 0;
